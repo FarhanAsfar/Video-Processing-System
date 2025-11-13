4117e58c20504c12a5a9ca8df003f2672f27046c
arnob <farhanasfar21@gmail.com>
2025-11-13 08:58:15 +0600

    Add multer middleware for file uploads

---
+++ api/src/middlewares/multer.js
@@ -0,0 +1,26 @@
+import express from "express"
+import multer from "multer"
+import fs from "fs"
+import path from "path"
+
+const tempDir = path.resolve("upload/");
+if(!fs.existsSync(tempDir)){
+    fs.mkdirSync(tempDir, {recursive: true});
+}
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, tempDir)
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9)
+        cb(null, uniqueSuffix + "-" + file.originalname);
+    }
+})
+
+const upload = multer({
+    storage,
+    limits: {fileSize: 5*1024*1024} //5mb limit
+})
+
+export {upload};
--- api/src/routes/video.routes.js
+++ api/src/routes/video.routes.js
@@ -1,12 +1,10 @@
 import {Router} from "express"
-import { videoQueue } from "../jobs/videoQueue.js"
-import { PrismaClient } from "@prisma/client"
 import { postVideo, getVideo } from "../controllers/video.controller.js";
 
 const videoRouter = Router();
 
 videoRouter.route('/').post(postVideo);
 videoRouter.route('/').get(getVideo)
-
+                                              
 
 export {videoRouter}
