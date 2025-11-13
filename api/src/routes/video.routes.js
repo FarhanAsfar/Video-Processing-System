2b7eacbf7b6380689fca94a21e358c6ea2b5ef45
arnob <farhanasfar21@gmail.com>
2025-11-13 09:03:08 +0600

    Add multer middleware in post route

--- api/src/routes/video.routes.js
+++ api/src/routes/video.routes.js
@@ -1,9 +1,10 @@
 import {Router} from "express"
 import { postVideo, getVideo } from "../controllers/video.controller.js";
+import {upload} from "../middlewares/multer.js"
 
 const videoRouter = Router();
 
-videoRouter.route('/').post(postVideo);
+videoRouter.route('/').post(upload.single("video"), postVideo);
 videoRouter.route('/').get(getVideo)
                                               
 
