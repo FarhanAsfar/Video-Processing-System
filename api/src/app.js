9f7e2dea93cbc612599d520b4ec3d94536b34dd6
arnob <farhanasfar21@gmail.com>
2026-07-14 00:55:43 +0600

    Add logging for API URL in server startup

--- api/src/app.js
+++ api/src/app.js
@@ -21,4 +21,5 @@ app.use(errorHandler);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port: ${process.env.PORT}`);
+    console.log(`API is running on port: ${process.env.API_URL}`);
 });
